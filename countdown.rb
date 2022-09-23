330451f69714e66c326013189cee5bb759471299
Luis <lvas248@gmail.com>
2022-09-23 16:19:33 -0400

    commit tree 923ae54105b647147656957d025e89fc948026b9

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown (int)
+    count = int
+    while count > 0
+        puts "#{count} SECOND(S)!"
+    count = count -1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (int)
+    count = int
+    while count > 0
+        sleep 1
+        puts "#{count} SECOND(S)!"
+    count = count -1
+    end
+    "HAPPY NEW YEAR!"
 end
